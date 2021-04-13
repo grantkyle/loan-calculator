5cbe42989d7e7916c66ecca5823c0fc6dbd57a36
Grant Kyle <sgrantkyle@gmail.com>
2021-04-13 16:24:52 -0500

    axios and rsuite ui library dependencies added

---
+++ src/components/Calculator/Calculator.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "rsuite/dist/styles/rsuite-default.min.css";
+
+function calculator() {
+    return (
+        <div>
+            calculator here
+        </div>
+    )
+}
+
+export default calculator
